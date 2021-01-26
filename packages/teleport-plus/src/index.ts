992aa284bf345670b87b21d170e5dd5c842d90c4
Thorsten Luenborg <t.luneborg@googlemail.com>
2021-01-26 08:43:03 +0100

    feat: plugin installs global components, optional own coordinator

--- packages/teleport-plus/src/index.ts
+++ packages/teleport-plus/src/index.ts
@@ -1,12 +1,5 @@
-import { App } from 'vue'
 import TeleportOutlet from './TeleportOutlet.vue'
 import TeleportSource from './TeleportSource.vue'
-import { createCoordinator, coordinatorKey } from './coordinator'
+export { install as default } from './install'
 
-export default function install(app: App) {
-  const coordinator = createCoordinator()
-  app.provide(coordinatorKey, coordinator)
-  console.log(coordinator)
-}
-
-export { install, TeleportOutlet, TeleportSource }
+export { TeleportOutlet, TeleportSource }
---
+++ packages/teleport-plus/src/install.ts
@@ -0,0 +1,30 @@
+import { App } from 'vue'
+import { createCoordinator, coordinatorKey, Coordinator } from './coordinator'
+import TeleportOutlet from './TeleportOutlet.vue'
+import TeleportSource from './TeleportSource.vue'
+
+export interface PluginOptions {
+  teleportOutlet?: string | false
+  teleportSource?: string | false
+  coordinator?: Coordinator
+}
+
+const defaultOptions: PluginOptions = {
+  teleportOutlet: 'TeleportOutlet',
+  teleportSource: 'TeleportSource',
+}
+
+export function install(app: App, options: PluginOptions = {}) {
+  const {
+    teleportOutlet,
+    teleportSource,
+    coordinator: _coordinator,
+  } = Object.assign({}, defaultOptions, options)
+
+  const coordinator = _coordinator ?? createCoordinator()
+
+  teleportSource && app.component(teleportSource, TeleportSource)
+  teleportOutlet && app.component(teleportOutlet, TeleportOutlet)
+
+  app.provide(coordinatorKey, coordinator)
+}
