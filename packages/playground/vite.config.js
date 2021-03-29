514f7045392cfb87dfd687940f41c50ffd83e48b
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-03-29 16:33:33 +0200

    chore: deps and config updates

--- packages/playground/vite.config.js
+++ packages/playground/vite.config.js
@@ -4,11 +4,13 @@ import WindiCSS from 'vite-plugin-windicss'
  * @type { import('vite').UserConfig }
  */
 export default {
-  alias: {
-    '@linusborg/vue-teleport-plus': '@linusborg/vue-teleport-plus/src/index.ts',
-  },
+  resolve: {},
   plugins: [vue(), WindiCSS()],
   resolve: {
+    alias: {
+      '@linusborg/vue-teleport-plus':
+        '@linusborg/vue-teleport-plus/src/index.ts',
+    },
     dedupe: ['vue'],
   },
   optimizeDeps: {
