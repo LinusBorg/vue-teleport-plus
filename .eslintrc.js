b0d13004c6fdfea731e9a2e4238e510ed52d7c6a
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-07-23 20:23:33 +0200

    chore: upgrade deps, fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   extends: ['@linusborg/eslint-config'],
+  rules: {
+    'vue/script-setup-uses-vars': 'warn',
+  },
   overrides: [
     {
       files: ['__tests__/**/*.spec.ts', '__tests__/**/*.spec.js'],
