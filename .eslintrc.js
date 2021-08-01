2f6638aabc856ac2db363c27af6be7db146ff3c1
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-08-01 21:31:15 +0200

    lint: prevent imports from vue sub-packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,17 @@ module.exports = {
   extends: ['@linusborg/eslint-config'],
   rules: {
     'vue/script-setup-uses-vars': 'warn',
+    'no-restricted-imports': [
+      'warn',
+      {
+        patterns: [
+          {
+            group: ['@vue/reactivity', '@vue/runtime-*', '@vue/shared'],
+            message: 'Import from package "vue" instead',
+          },
+        ],
+      },
+    ],
   },
   overrides: [
     {
